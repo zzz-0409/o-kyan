7eb1fa0894c24f1041f81408d1974756d1a66c37
zzz... <tsryt0409@gmail.com>
2026-07-30 09:55:16 +0900

    Widen runner road background

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "run-dash-cache-v10";
+const CACHE_NAME = "run-dash-cache-v11";
 const CORE_SHELL = [
   "./",
   "./index.html",
@@ -13,7 +13,7 @@ const APP_SHELL = [
   "./assets/ui/app-icon-512.png",
   "./assets/ui/title-bg-campus.png",
   "./assets/ui/result-panel-campus.png",
-  "./assets/backgrounds/imagegen-full-road-city-loop-wide.png",
+  "./assets/backgrounds/imagegen-full-road-city-loop-extra-wide.png",
   "./assets/backgrounds/imagegen-city-side-loop.png",
   "./assets/character/imagegen-runner-0.png",
   "./assets/character/imagegen-runner-1.png",
